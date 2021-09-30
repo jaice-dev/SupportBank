a3d294f73641824a317aa2cd1589d29384ad0997
Jordan <jordan.d.carlton@gmail.com>
2021-09-30 11:03:26 +0100

    init database class

---
+++ SupportBank/databses.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
